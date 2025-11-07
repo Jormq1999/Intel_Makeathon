5cfa0c460f2cea5667facf8ddb8fb1752c0d7edf
Salazar Sanchez, Juan <juan.salazar.sanchez@intel.com>
2025-11-07 10:55:06 -0800

    Juan las minute changes

--- Juan-local-testing/fifo_project/csrc/filelist.cu
+++ Juan-local-testing/fifo_project/csrc/filelist.cu
@@ -4,16 +4,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _19685_archive_1.so
-_19685_archive_1.so : archive.1/_19685_archive_1.a
+ARCHIVE_OBJS += _13590_archive_1.so
+_13590_archive_1.so : archive.6/_13590_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_19685_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_13590_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_19685_archive_1.so $@
+	@ln -sf .//../simv.daidir//_13590_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.1/_prev_archive_1.a
+_prev_archive_1.so : archive.6/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
